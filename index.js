b5d66ce96b3f9d4324d6daa7f80aa382ddcec267
uthmanmomen13 <livininthemomen13@gmail.com>
2021-01-21 18:30:41 -0800

    sum function

--- index.js
+++ index.js
@@ -15,6 +15,11 @@ client.on("message", function(message) {
         const timeTaken = Date.now() - message.createdTimestamp;
         message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
     }
+    else if (command === "sum") {
+        const numArgs = args.map(x => parseFloat(x));
+        const sum = numArgs.reduce((counter, x) => counter += x);
+        message.reply(`The sum of all the arguments you provided is ${sum}!`);
+      }
 });
 
 client.login(process.env.TOKEN);
