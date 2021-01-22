864861bc8360aeb221115af66e05ff39c5e49871
uthmanmomen13 <livininthemomen13@gmail.com>
2021-01-22 12:31:26 -0800

    using tf

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const Discord = require("discord.js");
+require('@tensorflow/tfjs');
+require('@tensorflow/tfjs-node');
+const toxicity = require('@tensorflow-models/toxicity');
 const client = new Discord.Client();
 require('dotenv').config();
 
 client.on("message", function(message) { 
+    const threshold = 0.9;
+    
     const prefix = "!";                
     if (message.author.bot) return;
-    if (!message.content.startsWith(prefix)) return;
+    if (!message.content.startsWith(prefix)){
+        toxicity.load(threshold).then(model =>{
+            const sentence = message.content;
+            model.classify(sentence).then(predictions => {
+                if(predictions[6].results[0].match) {
+                    message.reply('This message contains possible toxicity. Please watch your language.')
+                }
+            });
+        });
+    } 
 
     const commandBody = message.content.slice(1);
     const args = commandBody.split(' ');
